9caf07e809b1dda502dadbc919c92ed53e50e2fd
Adel Kharbout <Adel.Kharbout@ecube.de>
2024-04-29 12:11:01 +0200

    edited ProductDetails

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,20 +1,31 @@
-import {LoaderFunction, useParams} from "react-router-dom";
+import {LoaderFunction, useLoaderData} from "react-router-dom";
 import '../assets/ProductDetails.css';
+import {Product} from "./ProductList.tsx";
 
-const loader: LoaderFunction = async function getData() {
-    return fetch('http://localhost:3000/products')
-
+const loader: LoaderFunction = function getData({params}) {
+    const {productId} = params;
+    return fetch(`http://localhost:3000/products/${productId}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch product details');
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error('Error fetching product details:', error);
+            throw error;
+        });
 }
-export default function ProductDetails() {
 
-    const {productId} = useParams();
+export default function ProductDetails() {
+    const product: Product = useLoaderData() as Product;
 
     return (
         <>
             <h1 className={"product-details-container"}>ProductDetails</h1>
-            <p className={"product-details-content"}>ProductDetails for Product {productId}</p>
+            <p className={"product-details-content"}>ProductDetails for Product {product.id}</p>
+            <p> ProductDetails for Product {product.name}</p>
         </>
-
     )
 }
 
